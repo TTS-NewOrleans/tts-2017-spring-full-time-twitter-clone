abf9b09fd75bd6ed7f5d9ea79e1498d2f7694c96
Ryan Hageman <improv42@gmail.com>
2017-07-11 10:25:34 -0500

    Add Likes table and model. Set up model associations.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :tweets
+  has_many :likes
 
   # ~ Person who follows (current user) ~
   has_many :relationships
